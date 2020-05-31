b988afa249e880fdfa4738d8565fe4e60aff51e1
talk2div <56991349+talk2div@users.noreply.github.com>
2020-05-31 09:52:30 +0530

    initial commit
    This file created a table and inserted few entries in the specified table.

---
+++ my-simple-db.sql
@@ -0,0 +1,3 @@
+USE MyDB;
+CREATE TABLE MyTable (id integer, firstname varchar (50), lastname varchar(50));
+INSERT INTO MyTable (id, firstname, lastname) VALUES (1, “William”, “Shakespeare”), (2,”Bruce”, “Wayne”), (3,”Tony”,”Stark”);
